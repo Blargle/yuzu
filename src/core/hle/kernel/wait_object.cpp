603a8b099f2f37cd19ea365aa58f823b114402a9
Fernando Sahmkow <fsahmkow27@gmail.com>
2019-03-29 17:13:00 -0400

    Add PrepareReschedule where required.

--- src/core/hle/kernel/wait_object.cpp
+++ src/core/hle/kernel/wait_object.cpp
@@ -6,6 +6,8 @@
 #include "common/assert.h"
 #include "common/common_types.h"
 #include "common/logging/log.h"
+#include "core/core.h"
+#include "core/core_cpu.h"
 #include "core/hle/kernel/object.h"
 #include "core/hle/kernel/process.h"
 #include "core/hle/kernel/thread.h"
@@ -93,8 +95,12 @@ void WaitObject::WakeupWaitingThread(SharedPtr<Thread> thread) {
     if (thread->HasWakeupCallback())
         resume = thread->InvokeWakeupCallback(ThreadWakeupReason::Signal, thread, this, index);
 
-    if (resume)
+    if (resume) {
         thread->ResumeFromWait();
+        if (thread->GetProcessorID() >= 0)
+            Core::System::GetInstance().CpuCore(thread->GetProcessorID()).PrepareReschedule();
+    }
+
 }
 
 void WaitObject::WakeupAllWaitingThreads() {
